705382f64e61cad34fc979835dcafcacce6faa68
K-Kizuku <kotani.kizuku@gmail.com>
2023-12-08 13:30:24 +0900

    feat: 🍻 レスポンシブの対応

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,8 @@ module github.com/K-Kizuku/plesio-client/go
 
 go 1.21.0
 
-require gocv.io/x/gocv v0.35.0 // indirect
+require (
+	gocv.io/x/gocv v0.35.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+)
--- go/main.go
+++ go/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"image"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"gocv.io/x/gocv"
+	"golang.org/x/term"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt, os.Kill)
+	defer stop()
 	// colorset2 := "MWN$@%#&B89EGA6mK5HRkbYT43V0JL7gpaseyxznocv?jIftr1li*=-~^`':;,. "
 	// colorset3 := "MMMMMMMMMMMWWWWWNNN¶Ϡ$@%#&B89EGCA65HRKYT43Z2UV0O§JL7kbh1mwngpaseyxznocv?{}[]()|jIftrl!i*=<>+-~^\"`':;,,,.....                    "
 	// colorset4 := "MWNB89RKYUV0JL7kbh1mwngpasexznocv?{}()jftrl!i*=<>~^++--::::;;;;;;''''''``````,,,,,,..........                                   "
@@ -27,24 +35,39 @@ func main() {
 	defer gray.Close()
 	flame := gocv.NewMat()
 	defer flame.Close()
-	for {
-		if ok := video.Read(&flame); !ok {
-			// fmt.Print("エラーだよ")
-			return
-		}
-		if flame.Empty() {
-			continue
-		}
-		gocv.Resize(flame, &flame, image.Pt(100, 50), 0, 0, gocv.InterpolationNearestNeighbor)
-		gocv.CvtColor(flame, &gray, gocv.ColorBGRToGray)
-		output := ""
-		for i := 0; i < gray.Rows(); i++ {
-			for j := 0; j < gray.Cols(); j++ {
-				pixel := gray.GetUCharAt(i, j)
-				output += string(colorset[pixel/2]) + string(colorset[pixel/2])
+	fd := int(os.Stdout.Fd())
+	// fmt.Println(w, h)
+	// os.Exit(0)
+	go func(ctx context.Context) {
+		for {
+			w, h, err := term.GetSize(fd)
+			if err != nil {
+				fmt.Println("Error getting terminal size:", err)
 			}
-			output += "\n"
+			if ok := video.Read(&flame); !ok {
+				// fmt.Print("エラーだよ")
+				return
+			}
+			if flame.Empty() {
+				continue
+			}
+			gocv.Resize(flame, &flame, image.Pt(w/2, h-5), 0, 0, gocv.InterpolationNearestNeighbor)
+			gocv.CvtColor(flame, &gray, gocv.ColorBGRToGray)
+			output := ""
+			for i := 0; i < gray.Rows(); i++ {
+				for j := 0; j < gray.Cols(); j++ {
+					pixel := gray.GetUCharAt(i, j)
+					output += string(colorset[pixel/2]) + string(colorset[pixel/2])
+				}
+				output += "\n"
+			}
+			fmt.Printf("\033[%dA%s", h-5, output)
 		}
-		fmt.Printf("\033[50A%s", output)
-	}
+	}(ctx)
+
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	os.Exit(0)
 }
