67943380b23198fb62e9857feec2ed4b5448bcd3
Tanakaryuki <tnk3257.rt@gmail.com>
2023-12-17 02:08:04 +0900

    feat: 🍻 wip

--- python/main.py
+++ python/main.py
@@ -1,17 +1,27 @@
 import pyaudio
 import socket
 
-# Audio configuration
+from typing import Tuple
+
+import cv2
+import numpy as np
+import os
+import signal
+import sys
+import sounddevice as sd
+import socket
+import threading
+import json
+import base64
+
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-CHUNK_SIZE = 1024
+CHUNK_SIZE = 256
 
-# Network configuration
 DEST_IP = "127.0.0.1"  # Change to the intended destination IP
-DEST_PORT = 12345       # Change to the intended destination port
+DEST_PORT = 12345 
 
-# Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Initialize PyAudio
@@ -21,18 +31,107 @@ stream = audio_interface.open(format=FORMAT, channels=CHANNELS,
                               frames_per_buffer=CHUNK_SIZE)
 
 print("Sending audio to {}:{}".format(DEST_IP, DEST_PORT))
-try:
-    # Continuously record audio and send it
+
+
+def signal_handler(signum: int, frame) -> None:
+    sys.exit(0)
+
+colorset: str = "MWNB89RKYUV0JL7kbh1mwngpasexznocv?{}()jftrl!i*=<>~^++--::::;;;;;;\"\"\"\"\"\"''''''''````````````                                     "
+target_address = ('127.0.0.1', 12345)
+
+cap: cv2.VideoCapture = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 20)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+def get_terminal_size() -> Tuple[int, int]:
+    rows, cols = map(int, os.popen('stty size', 'r').read().split())
+    return rows, cols
+
+def udp_sender(data, address):
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    base64_data = base64.b64encode(data).decode('utf-8')
+    json_data = {
+                "type": "AA",
+                "header": {
+                    "room_id": "String",
+                    "want_client_id": "String",
+                },
+                "body": {
+                    "content":base64_data
+                }
+            }
+    json_str = json.dumps(json_data)
+    json_bytes = json_str.encode('utf-8')
+    message = udp_socket.sendto(json_bytes, address)
+    print(message)
+
+def audio_callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    udp_sender(indata.tobytes(), target_address)
+
+
+def main():
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    sd.default.samplerate = 4410
+    sd.default.channels = 1
+
+    count: int = 0
+
     while True:
+        h, w = get_terminal_size()
+        ret, frame = cap.read()
+
         data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
-        client_socket.sendto(data, (DEST_IP, DEST_PORT))
-        print("send")
-except KeyboardInterrupt:
-    # Stop and close everything properly
-    print("Interrupted by user")
-finally:
-    print("Closing stream and socket.")
-    stream.stop_stream()
-    stream.close()
-    audio_interface.terminate()
-    client_socket.close()
+        base64_data = base64.b64encode(data).decode('utf-8')
+        json_data = {
+            "type": "audio",
+            "header": {
+                "room_id": "String",
+                "want_client_id": "String",
+            },
+            "body": {
+                "content":base64_data
+            }
+        }
+        json_str = json.dumps(json_data)
+        json_bytes = json_str.encode('utf-8')
+        client_socket.sendto(json_bytes, (DEST_IP, DEST_PORT))
+
+        if not ret:
+            break
+
+        frame = cv2.resize(frame, (w // 2, h - 5))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        output: str = ""
+        for row in gray:
+            for pixel in row:
+                output += colorset[pixel // 2] + colorset[pixel // 2]
+            output += "\n"
+
+        count += 1
+        if(count == 60):
+            count = 0
+            os.system('clear')
+        print("\033[{}A{}".format(h - 5, output), end="")
+
+        threading.Thread(target=udp_sender, args=(output.encode(), target_address)).start()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Exit")
+        cap.release()
+        sd.stop()
+        print("Closing stream and socket.")
+        stream.stop_stream()
+        stream.close()
+        audio_interface.terminate()
+        client_socket.close()
--- python/udp.py
+++ python/udp.py
@@ -1,15 +1,20 @@
 import socket
 import pyaudio
+from typing import Tuple
+import os
+import json
+import base64
 
 # Audio configuration
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-CHUNK_SIZE = 1024
+CHUNK_SIZE = 256
 
-# Network configuration
-HOST = '0.0.0.0'  # Listen on all available interfaces
-PORT = 12345       # Port to listen on (should match the sender's port)
+count: int = 0
+
+HOST = '127.0.0.1'  # Listen on all available interfaces
+PORT = 1025       # Port to listen on (should match the sender's port)
 
 # Create a UDP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,19 +26,48 @@ stream = audio_interface.open(format=FORMAT, channels=CHANNELS,
                               rate=RATE, output=True,
                               frames_per_buffer=CHUNK_SIZE)
 
-print("Listening on {}:{}".format(HOST, PORT))
-try:
-    # Continuously receive audio and play it
-    while True:
-        data, addr = server_socket.recvfrom(65535)
-        stream.write(data)
-except KeyboardInterrupt:
-    # Stop and close everything properly
-    print("Interrupted by user")
-finally:
-    print("Closing stream and socket.")
-    stream.stop_stream()
-    stream.close()
-    audio_interface.terminate()
-    server_socket.close()
+def get_terminal_size() -> Tuple[int, int]:
+    rows, cols = map(int, os.popen('stty size', 'r').read().split())
+    return rows, cols
 
+def udp_server(host, port):
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((host, port))
+
+    print(f"UDP Server listening on {host}:{port}")
+    count: int = 0
+
+    while True:
+        print("AAhijjs")
+        data, addr = server_socket.recvfrom(0)
+        print("AA")
+        try:
+            json_data = json.loads(data.decode('utf-8'))
+            print(json_data)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            print(data)
+
+        if 'type' in json_data:
+            message_type = json_data['type']
+            if message_type == "AA":
+                data = json_data["body"]["content"]
+                data = base64.b64decode(data.decode('utf-8'))
+                h, w = get_terminal_size()
+                count += 1
+                if(count == 60):
+                    count = 0
+                    os.system('clear')
+                print("\033[{}A{}".format(h - 5, data), end="")
+            elif(message_type == "audio"):
+                stream.write(json_data["body"]["content"])
+            else:
+                print("ERROR")
+        else:
+            print("Error: 'type' field not found in the received JSON data.")
+
+if __name__ == "__main__":
+    host = '0.0.0.0'  # 受け付けるIPアドレス (0.0.0.0はすべてのアドレスを表します)
+    port = 12345       # 受け付けるポート番号 (Senderと合わせてください)
+
+    udp_server(host, port)
