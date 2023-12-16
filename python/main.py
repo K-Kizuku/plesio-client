76da01fdc2bc51b2396d079a3e0a18a2b9c098a8
rockcutter <rockcutter414@gmail.com>
2023-12-16 22:45:19 +0900

    perf: 🚀 音声通話を途切れることなく

--- python/main.py
+++ python/main.py
@@ -1,82 +1,38 @@
-from typing import Tuple
-
-import cv2
-import numpy as np
-import os
-import signal
-import sys
-import sounddevice as sd
+import pyaudio
 import socket
-import threading
 
-def signal_handler(signum: int, frame) -> None:
-    sys.exit(0)
+# Audio configuration
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK_SIZE = 1024
 
-colorset: str = "MWNB89RKYUV0JL7kbh1mwngpasexznocv?{}()jftrl!i*=<>~^++--::::;;;;;;\"\"\"\"\"\"''''''''````````````                                     "
-target_address = ('127.0.0.1', 1025)
+# Network configuration
+DEST_IP = "127.0.0.1"  # Change to the intended destination IP
+DEST_PORT = 12345       # Change to the intended destination port
 
-cap: cv2.VideoCapture = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 20)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def get_terminal_size() -> Tuple[int, int]:
-    # rows, cols = map(int, os.popen('stty size', 'r').read().split())
-    size = os.get_terminal_size()
-    lines = size.lines
-    columns = size.columns
+# Initialize PyAudio
+audio_interface = pyaudio.PyAudio()
+stream = audio_interface.open(format=FORMAT, channels=CHANNELS,
+                              rate=RATE, input=True,
+                              frames_per_buffer=CHUNK_SIZE)
 
-    return lines, columns
-
-def udp_sender(data, address):
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.sendto(data, address)
-
-def audio_callback(indata, frames, time, status):
-    if status:
-        print(status, file=sys.stderr)
-    udp_sender(indata.tobytes(), target_address)
-
-def main():
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
-
-    sd.default.samplerate = 4410
-    sd.default.channels = 1
-
-    count: int = 0
-
-    with sd.InputStream(callback=audio_callback):
-        while True:
-            h, w = get_terminal_size()
-            ret, frame = cap.read()
-
-            if not ret:
-                break
-
-            frame = cv2.resize(frame, (w // 2, h - 5))
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            output: str = ""
-            for row in gray:
-                for pixel in row:
-                    output += colorset[pixel // 2] + colorset[pixel // 2]
-                output += "\n"
-
-            count += 1
-            if(count == 60):
-                count = 0
-                os.system('clear')
-            print("\033[{}A{}".format(h - 5, output), end="")
-
-            threading.Thread(target=udp_sender, args=(output.encode(), target_address)).start()
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Exit")
-        cap.release()
-        sd.stop()
+print("Sending audio to {}:{}".format(DEST_IP, DEST_PORT))
+try:
+    # Continuously record audio and send it
+    while True:
+        data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+        client_socket.sendto(data, (DEST_IP, DEST_PORT))
+        print("send")
+except KeyboardInterrupt:
+    # Stop and close everything properly
+    print("Interrupted by user")
+finally:
+    print("Closing stream and socket.")
+    stream.stop_stream()
+    stream.close()
+    audio_interface.terminate()
+    client_socket.close()
--- python/udp.py
+++ python/udp.py
@@ -1,33 +1,39 @@
 import socket
-import os
-from typing import Tuple
-import os
-import time
+import pyaudio
 
-def get_terminal_size() -> Tuple[int, int]:
-    rows, cols = map(int, os.popen('stty size', 'r').read().split())
-    return rows, cols
+# Audio configuration
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK_SIZE = 1024
 
-def udp_server(host, port):
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind((host, port))
+# Network configuration
+HOST = '0.0.0.0'  # Listen on all available interfaces
+PORT = 12345       # Port to listen on (should match the sender's port)
 
-    print(f"UDP Server listening on {host}:{port}")
-    count: int = 0
+# Create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((HOST, PORT))
 
+# Initialize PyAudio
+audio_interface = pyaudio.PyAudio()
+stream = audio_interface.open(format=FORMAT, channels=CHANNELS,
+                              rate=RATE, output=True,
+                              frames_per_buffer=CHUNK_SIZE)
+
+print("Listening on {}:{}".format(HOST, PORT))
+try:
+    # Continuously receive audio and play it
     while True:
-        data, addr = udp_socket.recvfrom(8192)
-        h, w = get_terminal_size()
-        count += 1
-        if(count == 60):
-            count = 0
-            os.system('clear')
-        # os.system('clear')
-        # print(f"Received data from {addr}: {data}")
-        print("\033[{}A{}".format(h - 5, data.decode()), end="")
+        data, addr = server_socket.recvfrom(65535)
+        stream.write(data)
+except KeyboardInterrupt:
+    # Stop and close everything properly
+    print("Interrupted by user")
+finally:
+    print("Closing stream and socket.")
+    stream.stop_stream()
+    stream.close()
+    audio_interface.terminate()
+    server_socket.close()
 
-if __name__ == "__main__":
-    host = '127.0.0.1'  # 受け付けるIPアドレス (0.0.0.0はすべてのアドレスを表します)
-    port = 1025       # 受け付けるポート番号 (Senderと合わせてください)
-
-    udp_server(host, port)
